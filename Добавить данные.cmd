c32b72dafb6e0a536f4a8fb1dc048a0aba260806
alex-dev-2020 <alex_dev_2020@mail.ru>
2023-04-14 21:24:06 +1000

    Добавить данные  initial commit

---
+++ Добавить данные.cmd
@@ -0,0 +1,4 @@
+
+D:\SSL\1C\Install\x86curl\bin\curl.exe -d "{\"Code\": \"000000001\",\"Description\": \"Product_01\"}" -u admin:admin http://localhost:8081/InfoBase/odata/standard.odata/Catalog_Product?$format=json  
+
+pause
